2b9df0f88e23d0235325dc119935ad820cd32735
paulius <arxenaut@gmail.com>
2021-03-01 21:00:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main () {
     try {
-    user::User a, b;
-    a.setNickname("Name");
-    a.setPassword("P4$$word");
-    a.setEmail("admin@pinigenai.lt");
-    b.setNickname("pineapple911");
-    b.setRandomPassword();
-    b.setEmail("pewdiepie@gmail.com");
-    cout << a.toString() << endl << b.toString();
-    b.setPassword("Password123"); // exception (no special character)
+        user::User a, b;
+        a.setNickname("Name");
+        a.setPassword("P4$$word");
+        a.setEmail("admin@pinigenai.lt");
+        b.setNickname("pineapple911");
+        b.setRandomPassword();
+        b.setEmail("pewdiepie@gmail.com");
+        cout << a.toString() << endl << b.toString();
+        b.setPassword("Password123"); // exception (no special character)
     }
-    catch (string err) {
-        cout << err << endl;
+    catch (invalid_argument &err) {
+        cout << err.what() << endl;
     }
     catch (...) {
         cout << "Unknown exception" << endl;
